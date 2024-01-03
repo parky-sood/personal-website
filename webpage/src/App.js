1b5ee290af1ab719f9903d998a3cc366de2b0f1e
Parikshit Sood <parkysood@gmail.com>
2024-01-02 22:09:04 -0800

    navbar unstyled

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
+import linkedin from './linkedin.svg';
+import github from './github.svg'
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      {navbar()}
     </div>
   );
+}
+
+function navbar() {
+  return (
+      <nav className='navbar'>
+        <ul className='primary-nav'>
+          <li className='prim-nav-item'>Home</li>
+          <li className='prim-nav-item'>Projects</li>
+          <li className='prim-nav-item'>About Me</li>
+          <li className='prim-nav-item'>Contact</li>
+        </ul>
+        <ul className='secondary-nav'>
+          <li className='sec-nav-item'>
+            <a className = 'LinkedIn'
+               href='https://www.linkedin.com/in/parikshit-sood'
+               target='_blank'>
+               <img src={linkedin} className='linkedin-logo' alt='linkedin'/>
+            </a>
+          </li>
+          <li className='sec-nav-item'>
+            <a className='GitHub'
+               href='https://github.com/parky-sood'
+               target='_blank'>
+               <img src={github} className='github-logo' alt='github'/>
+            </a>
+          </li>
+        </ul>
+      </nav>
+  );
+}
+
+function home() {
+
+}
+
+function projects() {
+
+}
+
+function about() {
+
+}
+
+function contact() {
+
 }
 
 export default App;
